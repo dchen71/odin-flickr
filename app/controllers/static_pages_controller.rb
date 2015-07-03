8d9e0c2e28c804c71c37c9731e9d36f176602949
Dan <dchen12812@gmail.com>
2015-07-02 21:57:45 -0700

    testing through heroku deployment

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,10 @@
 class StaticPagesController < ApplicationController
+  def index
+    if params[:flickr_id]
+      @flickr_id = params[:flickr_id]
+    else
+      @flickr_id = "133938247@N06"
+    end
+  end
+	
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#uses the flickraw gem
+require 'flickraw'
+
+FlickRaw.api_key = ENV["flickr_key"]
+FlickRaw.shared_secret = ENV["flickr_secret"]
+
